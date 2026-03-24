4833b87f33781fece4fcb384132c321dfe041567
HuzefaPainter <painterhuzefa@gmail.com>
2026-03-24 18:59:19 +0530

    router workaround removal

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -5,9 +5,7 @@
 
   bookingRouter.post("/book-show", bookShow);
 
-  //bookingRouter.post("/payment-success", paymentSuccess);
-  // Temporary workaround
-  bookingRouter.get("/payment-success", paymentSuccess);
+  bookingRouter.post("/payment-success", paymentSuccess);
 
   bookingRouter.post("/payment-failure", paymentFailure);
 
