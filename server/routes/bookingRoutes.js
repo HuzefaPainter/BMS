e86ba3a4390d08cd502f75994c36dae77894eda9
HuzefaPainter <painterhuzefa@gmail.com>
2026-03-24 17:55:35 +0530

    Payment get fix

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -5,7 +5,9 @@
 
   bookingRouter.post("/book-show", bookShow);
 
-  bookingRouter.post("/payment-success", paymentSuccess);
+  //bookingRouter.post("/payment-success", paymentSuccess);
+  // Temporary workaround
+  bookingRouter.get("/payment-success", paymentSuccess);
 
   bookingRouter.post("/payment-failure", paymentFailure);
 
