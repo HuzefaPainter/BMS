eca8c6fcea2df13c498f3f1a28e1ed5398a60ac6
HuzefaPainter <painterhuzefa@gmail.com>
2026-03-25 15:17:51 +0530

    Security additions

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require("./config/dbconfig");
-
+import { rateLimit } from 'express-rate-limit'
+const bodyParser = require('body-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+import helmet from "helmet";
 
 const userRoute = require("./routes/userRoutes");
 const movieRoute = require("./routes/movieRoutes");
@@ -13,7 +16,20 @@ const bookingRoute = require("./routes/bookingRoutes");
 const transactionRoute = require("./routes/transactionRoutes");
 const startCleanupJob = require("./jobs/cleanupExpiredBookings");
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window`
+	standardHeaders: 'draft-8',
+	legacyHeaders: false,
+	ipv6Subnet: 56,
+})
 
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+);
+app.use(limiter)
 app.use(cors({
   origin: process.env.frontend_domain,
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -21,6 +37,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(mongoSanitize());
 app.use("/", userRoute);
 app.use("/", movieRoute);
 app.use("/", theatreRoute);
